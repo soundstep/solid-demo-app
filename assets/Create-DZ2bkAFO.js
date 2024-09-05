ec5a1a021ad62b7d3fb37a5d3a0fa6a65c7adcd8
Romuald Quantin <romu@soundstep.com>
2024-09-05 11:38:59 +0100

    Updates

---
+++ assets/Create-DZ2bkAFO.js
@@ -0,0 +1,220 @@
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
+import { h as r, g as v, c as d, i as e, V as n, T as i, k as f, S as P, s as B, l as I, m as j } from "./index-CMXG2PLb.js";
+import "./CoreExtension-DUlHFTtP.js";
+var M = function M() {
+  var a = {
+      width: 900,
+      height: 500,
+      y: 50,
+      x: 150,
+      gap: 25,
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "flexStart",
+      color: r("00000000")
+    },
+    u = {
+      width: 900,
+      height: 36,
+      gap: 6,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "flexStart",
+      color: r("00000000")
+    },
+    g = {
+      fontSize: 42
+    },
+    c = {
+      fontSize: 26
+    };
+  var l, s;
+  v(function () {
+    B("#000000"), l.animate({
+      x: 100
+    }, {
+      duration: 2e3
+    }).start();
+  });
+  var _d = d(),
+    _d2 = _slicedToArray(_d, 2),
+    h = _d2[0],
+    y = _d2[1],
+    _d3 = d(),
+    _d4 = _slicedToArray(_d3, 2),
+    m = _d4[0],
+    k = _d4[1];
+  setTimeout(function () {
+    var t;
+    y("- Inserted -"), (t = s.getChildById("child1")) == null || t.animate({
+      x: 600
+    }, {
+      duration: 2e3
+    }).start();
+  }, 2e3);
+  var w = {
+      width: 400,
+      height: 300,
+      color: r("#0000ff")
+    },
+    x = {
+      color: r("#f54242"),
+      focus: {
+        color: r("#4287f5")
+      }
+    };
+  function T(t) {
+    var o = I(function () {
+      return t.children;
+    });
+    return e(n, j(t, {
+      get style() {
+        return [x, t.style, [w]];
+      },
+      get children() {
+        return [e(n, {
+          id: "child1",
+          width: 100,
+          height: 100,
+          get color() {
+            return r("#ff0000");
+          },
+          y: 25,
+          get children() {
+            return [f(function () {
+              return o();
+            }), e(n, {
+              id: "subChild",
+              x: 150,
+              width: 100,
+              height: 100,
+              get color() {
+                return r("#00ff00");
+              }
+            }), e(i, {
+              get children() {
+                return t.title;
+              }
+            })];
+          }
+        }), e(n, {
+          width: 100,
+          height: 100,
+          get color() {
+            return r("#ffff00");
+          },
+          y: 175,
+          get children() {
+            return o();
+          }
+        })];
+      }
+    }));
+  }
+  var S = {
+      borderLeft: {
+        width: 8,
+        color: 95598118
+      },
+      borderTop: {
+        width: 8,
+        color: 631422246
+      },
+      borderRight: {
+        width: 8,
+        color: 95598118
+      },
+      borderBottom: {
+        width: 8,
+        color: 3316790822
+      }
+    },
+    p = {
+      color: r("#cccccc"),
+      focus: {
+        color: r("#000000")
+      }
+    },
+    C = {
+      color: r("#ffffff"),
+      focus: {
+        color: r("#f6f6cc")
+      }
+    };
+  function b(t) {
+    return t.states.has("focus");
+  }
+  return e(n, {
+    style: a,
+    get children() {
+      return [e(i, {
+        style: g,
+        children: "Title of the Page"
+      }), e(n, {
+        style: u,
+        get children() {
+          return [e(i, {
+            get children() {
+              return m();
+            }
+          }), e(i, {
+            style: c,
+            get children() {
+              return ["Sub ", f(function () {
+                return h();
+              }), " Text"];
+            }
+          }), e(P, {
+            get when() {
+              return h();
+            },
+            get children() {
+              return e(n, {
+                width: 28,
+                height: 28,
+                src: "./assets/rt-popcorn.png"
+              });
+            }
+          }), e(i, {
+            style: c,
+            children: "More Text"
+          })];
+        }
+      }), e(T, {
+        autofocus: !0,
+        ref: function ref(t) {
+          var o = s;
+          typeof o == "function" ? o(t) : s = t;
+        },
+        style: [C, p],
+        get children() {
+          return e(i, {
+            children: "Child Test"
+          });
+        }
+      }), e(n, {
+        ref: function ref(t) {
+          var o = l;
+          typeof o == "function" ? o(t) : l = t;
+        },
+        style: S,
+        width: 100,
+        height: 100,
+        get color() {
+          return r("#00ff00");
+        },
+        x: 900,
+        y: 400,
+        get alpha() {
+          return b(l) ? 1 : .2;
+        }
+      })];
+    }
+  });
+};
+export { M as default };
---
+++ assets/People-CsrA-CQV.js
@@ -0,0 +1,141 @@
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _slicedToArray(r, e) { return _arrayWithHoles(r) || _iterableToArrayLimit(r, e) || _unsupportedIterableToArray(r, e) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
+function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function _iterableToArrayLimit(r, l) { var t = null == r ? null : "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (null != t) { var e, n, i, u, a = [], f = !0, o = !1; try { if (i = (t = t.call(r)).next, 0 === l) { if (Object(t) !== t) return; f = !1; } else for (; !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l); f = !0); } catch (r) { o = !0, n = r; } finally { try { if (!f && null != t["return"] && (u = t["return"](), Object(u) !== u)) return; } finally { if (o) throw n; } } return a; } }
+function _arrayWithHoles(r) { if (Array.isArray(r)) return r; }
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+import { y as g, z as p, A as m, B as y, u as f, a as l, h as w, g as b, i as e, S as c, V as a, j as o, T as i, t as C, C as x, G as T, s as k } from "./index-CMXG2PLb.js";
+import { a as I } from "./CoreExtension-DUlHFTtP.js";
+function B(_ref) {
+  var r = _ref.id;
+  return g.get("/person/".concat(r, "/combined_credits")).then(function (_ref2) {
+    var t = _ref2.cast;
+    return p(t.slice(0, 7));
+  });
+}
+function R(_ref3) {
+  var r = _ref3.id;
+  return g.get("/person/".concat(r)).then(function (t) {
+    return _objectSpread({
+      backgroundImage: m(t.profile_path, "original"),
+      heroContent: {
+        title: t.title || t.name,
+        description: t.biography
+      }
+    }, t);
+  });
+}
+var F = function F() {
+  var r = y(),
+    t = f(),
+    _l = l(function () {
+      return _objectSpread({}, r);
+    }, R),
+    _l2 = _slicedToArray(_l, 1),
+    n = _l2[0],
+    _l3 = l(function () {
+      return _objectSpread({}, r);
+    }, B),
+    _l4 = _slicedToArray(_l3, 1),
+    u = _l4[0],
+    h = {
+      color: w("#000000"),
+      alpha: .8,
+      width: 800,
+      height: 440,
+      x: 130,
+      y: 180,
+      borderRadius: 30
+    };
+  function d() {
+    var s = this.children.selected;
+    I(s && s.href), t(s.href);
+  }
+  return b(function () {
+    k("#333333");
+  }), e(c, {
+    get when() {
+      return n();
+    },
+    keyed: !0,
+    get children() {
+      return [e(a, {
+        get src() {
+          return n().backgroundImage;
+        },
+        width: 400,
+        autosize: !0,
+        y: 0,
+        x: 1800,
+        mountX: 1
+      }), e(a, {
+        x: 150,
+        y: 200,
+        width: 800,
+        gap: 24,
+        get style() {
+          return o.Column;
+        },
+        zIndex: 3,
+        get children() {
+          return [e(i, {
+            contain: "width",
+            fontFamily: "Ubuntu",
+            get style() {
+              return C.typography.display2;
+            },
+            get children() {
+              return n().name;
+            }
+          }), e(i, {
+            contain: "both",
+            get style() {
+              return o.peopleBio;
+            },
+            get children() {
+              return n().biography;
+            }
+          })];
+        }
+      }), e(a, {
+        style: h
+      }), e(x, {
+        y: 670,
+        x: 140,
+        get style() {
+          return o.Column;
+        },
+        scroll: "none",
+        get children() {
+          return e(c, {
+            get when() {
+              return u();
+            },
+            get children() {
+              return [e(i, {
+                skipFocus: !0,
+                get style() {
+                  return o.RowTitle;
+                },
+                children: "Credits"
+              }), e(T, {
+                autofocus: !0,
+                onEnter: d,
+                get items() {
+                  return u();
+                }
+              })];
+            }
+          });
+        }
+      })];
+    }
+  });
+};
+export { F as default };
